77996f91c565b1744fb5c90c7ba4ae92acb0c539
zhuxiang <787681084@qq.com>
2019-01-15 17:52:37 +0800

    微服务学习

---
+++ message-thrift-service/src/main/java/com/imooc/message/MessageThriftServiceApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageThriftServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageThriftServiceApplication.class, args);
+    }
+
+}
+
