e6b102cda3e248e1d6d6bac31f07f351c46b16d2
Zhu Xiang <787681084@qq.com>
2020-02-21 16:25:36 +0800

    更新main方法

--- user-service/src/main/java/com/cloud/user/UserServiceApplication.java
+++ user-service/src/main/java/com/cloud/user/UserServiceApplication.java
@@ -17,7 +17,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableCircuitBreaker
 public class UserServiceApplication {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
     }
 }
